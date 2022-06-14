c5b6b02b534a3544416f90d181fd987f302af59c
Gabriel Gonzales <106750788+GabrielGonzalesKZ@users.noreply.github.com>
2022-06-14 17:20:37 -0300

    atividade javascript

---
+++ sript.js
@@ -0,0 +1,9 @@
+let nome             = "Gabriel"
+let sobrenome        = "Gonzales"
+let anoNascimento    = 1997
+let cidade           = "Curitiba"
+let anoAtual         = 2022
+let idade            = anoAtual - anoNascimento
+
+console.log (`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}.` )
+
